15d02deff5aa530d17e7f7a0a3a30d405c06f349
Alexey <alexeykapanov@gmail.com>
2017-01-26 15:09:31 +0300

    Fixed conflict

--- codeblog.sharingbasket/install/version.php
+++ codeblog.sharingbasket/install/version.php
@@ -1,7 +1,3 @@
 <?
 $arModuleVersion = array('VERSION'      => '0.3',
-<<<<<<< HEAD
                          'VERSION_DATE' => '2017-01-26 16:32:00');
-=======
-                         'VERSION_DATE' => '2017-01-25 16:32:00');
->>>>>>> 89399636ce24544434b69c2208e75912d16b66b7
