99cfd210e239252793521c1e7adc0ad26495265e
Alexey Panov <alexeykapanov@gmail.com>
2017-05-23 15:10:08 +0300

    Fixed language file for installing

--- codeblog.sharingbasket/lang/ru/install/index.php
+++ codeblog.sharingbasket/lang/ru/install/index.php
@@ -1,8 +1,15 @@
 <?
-$MESS['CODEBLOG_PARTNER']                            = 'CodeBlogPro';
-$MESS['CODEBLOG_PARTNER_URI']                        = 'http://codeblog.pro';
-$MESS['CODEBLOG_SHARING_BASKET_INSTALL_NAME']        = 'Сохранение и восстановление корзин';
-$MESS['CODEBLOG_SHARING_BASKET_INSTALL_DESCRIPTION'] = 'Модуль позволяет сохранять содержимое корзины, передавать другому пользователю и восстанавливать её впоследствии по коду';
-$MESS['CODEBLOG_SHARING_BASKET_INSTALL_TITLE']       = 'Установка модуля &laquo;Сохранение и восстановление корзин&raquo;';
-$MESS['CODEBLOG_SHARING_BASKET_UNINSTALL_TITLE']     = 'Удаление модуля &laquo;Сохранение и восстановление корзин&raquo;';
-$MESS['CODEBLOG_SHARING_BASKET_MODULE_REQUIRED']     = 'Для установки данного решения необходимо наличие модуля Сохранение и восстановление корзин.';
+$MESS['CODEBLOG_PARTNER']                                      = 'CodeBlogPro';
+$MESS['CODEBLOG_PARTNER_URI']                                  = 'http://codeblog.pro';
+$MESS['CODEBLOG_SHARING_BASKET_INSTALL_NAME']                  = 'Сохранение и восстановление корзин';
+$MESS['CODEBLOG_SHARING_BASKET_INSTALL_DESCRIPTION']           = 'Модуль позволяет сохранять содержимое корзины, передавать другому пользователю и восстанавливать её впоследствии по коду';
+$MESS['CODEBLOG_SHARING_BASKET_INSTALL_TITLE']                 = 'Установка модуля &laquo;Сохранение и восстановление корзин&raquo;';
+$MESS['CODEBLOG_SHARING_BASKET_UNINSTALL_TITLE']               = 'Удаление модуля &laquo;Сохранение и восстановление корзин&raquo;';
+$MESS['CODEBLOG_SHARING_BASKET_MODULE_REQUIRED']               = 'Для установки данного решения необходимо наличие модуля Сохранение и восстановление корзин.';
+$MESS['CODEBLOG_BASKET_SHARING_YOUR_VERSION_PHP']              = 'Ваша версия PHP';
+$MESS['CODEBLOG_BASKET_SHARING_YOUR_IS_OLD_VERSION_PHP']       = 'Внимание! Для продолжения установки модуля обновите версию PHP до 5.4 или более поздней версии.';
+$MESS['CODEBLOG_BASKET_SHARING_CHOOSE_STORAGE_BASKET']         = 'Выберите тип хранилища для корзин пользователей';
+$MESS['CODEBLOG_BASKET_SHARING_STORAGE_IBLOCK_VALUE']          = 'Инфоблок';
+$MESS['CODEBLOG_BASKET_SHARING_STORAGE_HIGHLOAD_IBLOCK_VALUE'] = 'Bitrix ХайлоадБлок';
+$MESS['CODEBLOG_BASKET_SHARING_STORAGE_MYSQL_TABLE_VALUE']     = 'Отдельная таблица в MySQL';
+$MESS['CODEBLOG_BASKET_SHARING_FORM_SUBMIT_VALUE']             = 'Подтвердить';
